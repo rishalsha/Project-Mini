96d89a9ce61942f5e313cd92a9f49d24179976e0
Muhammad Rishal P <rishal.p786@gmail.com>
2025-12-29 19:17:45 +0530

    Stable V1.0

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     server: {
-      port: 3210,
+      port: 3500,
       host: '0.0.0.0',
     },
     plugins: [react()],
